a3aaaafdfd4840a100f6b2b9ecb30a3392fe672b
Tom Helmuth <helmutht@wlu.edu>
2016-08-09 23:07:43 -0400

    Updated to latest Clojush

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clojush "2.3.1"]]
+                 [clojush "2.4.0"]]
   :main ^:skip-aot simplification-experiments.core)
